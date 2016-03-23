2b1d02ce9661fb0a078e280b1c009c2aba941895
Lachlan Donald <lachlan@ljd.cc>
2016-03-23 15:53:03 +1100

    Build script references correct make target

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -3,4 +3,4 @@ set -eux
 docker run --rm \
   --volume "$PWD":/go/src/github.com/buildkite/buildkite-cloudwatch-metrics-publisher \
   --workdir /go/src/github.com/buildkite/buildkite-cloudwatch-metrics-publisher \
-  golang:1.6.0 make build-cli
+  golang:1.6.0 make build
